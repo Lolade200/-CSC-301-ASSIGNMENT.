9d7bc63edb753a63b230627a82513be47239f332
unknown <sa9362673@gmail.com>
2025-12-03 13:12:52 +0100

    first commit

---
+++ classwork5.py
@@ -0,0 +1,15 @@
+# ==============================================================================
+#                  DATA STRUCTURES LABORATORY REPORT
+#          IMPLEMENTATION OF SINGLY LINKED LIST & THEORY ANSWERS
+# ==============================================================================
+# Submitted by: [AJIBOSE AFOLABI AMOS]
+# Matric No:    [230502010]
+# Department:   Computer Science
+# course code:        CSC 301
+# course title:      Data Structures
+# ==============================================================================
+
+
+q = []
+q.append(10)     # enqueue
+x = q.pop(0)     # dequeue -> O(n) because shift of all elemen
